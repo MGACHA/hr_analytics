de96153e965aecdd515ee86c00f051a68babd911
MGACHA <71405661+MGACHA@users.noreply.github.com>
2026-03-24 21:50:37 +0000

    Create hr_analytics database with initial setup

---
+++ create_hr_analytics.sql
@@ -0,0 +1,19 @@
+create database hr_analytics
+
+go
+
+use hr_analytics
+
+go
+
+
+/*-- DROP database
+USE master;
+GO
+
+ALTER DATABASE hr_analytics SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+GO
+
+DROP DATABASE hr_analytics;
+GO
+*/
